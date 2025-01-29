891f71de6fede35fecf9015ec34c3f62a41804cc
avangardewashere <avelpanaligan@gmail.com>
2025-01-30 01:27:48 +0800

    add/editor-props

--- src/app/documents/[documentId]/Editor.tsx
+++ src/app/documents/[documentId]/Editor.tsx
@@ -1,15 +1,23 @@
+"use client";
 import { useEditor, EditorContent } from "@tiptap/react";
 import StarterKit from "@tiptap/starter-kit";
 
 export const Editor = () => {
   const editor = useEditor({
+    editorProps: {
+      attributes: {
+        class: "focus:outline-none",
+      },
+    },
     extensions: [StarterKit],
     content: "<p>Hello Worlds</p>",
   });
 
   return (
-    <div>
-      <EditorContent editor={editor} />
+    <div className="size-full overflow-x-auto bg-[#F9FBFD] px-4 print:p-0 print:bg-white print:overflow-visible">
+      <div className="min-w max flex justify-center w-[816px] py-4 print:pu-0 mx-auto print:w-full print:min-w-0">
+        <EditorContent editor={editor} />
+      </div>
     </div>
   );
 };
--- src/app/documents/[documentId]/page.tsx
+++ src/app/documents/[documentId]/page.tsx
@@ -1,12 +1,18 @@
 import React from "react";
+import { Editor } from "./Editor";
 
 interface DocumentIdPageProps {
   params: Promise<{ documentId: string }>;
 }
 
 const DocumentsSinglePage = async ({ params }: DocumentIdPageProps) => {
-  const {documentId} = await params;
-  return <div>{documentId}</div>;
+  const { documentId } = await params;
+  return (
+    <div className="bg-[#F9FBFD]">
+      {documentId}
+      <Editor />
+    </div>
+  );
 };
 
 export default DocumentsSinglePage;
