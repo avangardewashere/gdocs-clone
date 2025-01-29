02715aca567a78085364ef5a03ef5caba1582688
avangardewashere <avelpanaligan@gmail.com>
2025-01-30 01:19:06 +0800

    add/link-documents

--- src/app/documents/layout.tsx
+++
@@ -1,9 +0,0 @@
-interface DOcumentsLayoutProps {
-  children: React.ReactNode;
-}
-
-const DocumentsLayout = ({ children }: DOcumentsLayoutProps) => {
-  return <div>Doc{children}</div>;
-};
-
-export default DocumentsLayout
--- src/app/documents/page.tsx
+++ src/app/documents/page.tsx
@@ -1,11 +1,18 @@
-import React from 'react'
+import Link from "next/link";
+import React from "react";
 
 const DocumentsPage = () => {
   return (
     <div>
-      
-    </div>
-  )
-}
+      Click{" "}
+      <Link href="/documents/test" className="mx-2 text-sky-400">
+        
+        here
+      </Link>
 
-export default DocumentsPage
+      to go to documents Id Page
+    </div>
+  );
+};
+
+export default DocumentsPage;
