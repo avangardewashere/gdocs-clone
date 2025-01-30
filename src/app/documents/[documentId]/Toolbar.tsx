6f3911f2609314d9b74f64e47180b69b0d50556e
avangardewashere <avelpanaligan@gmail.com>
2025-01-30 17:13:26 +0800

    fix/heading-error

--- src/app/documents/[documentId]/Toolbar.tsx
+++ src/app/documents/[documentId]/Toolbar.tsx
@@ -21,12 +21,69 @@ import {
   Undo2Icon,
 } from "lucide-react";
 import React from "react";
-
+import {type Level} from "@tiptap/extension-heading"
 interface ToolbarButtonProps {
   onClick?: () => void;
   isActive: boolean;
   icon: LucideIcon;
 }
+
+const HeadlingLevelButton = () => {
+  const { editor } = useEditorStore();
+
+  const headings = [
+    { label: "Normal text", value: 0, fontSize: "16px" },
+    { label: "Heading 1", value: 1, fontSize: "32px" },
+    { label: "Heading 2", value: 2, fontSize: "24px" },
+    { label: "Heading 3", value: 3, fontSize: "20px" },
+    { label: "Heading 4", value: 4, fontSize: "18px" },
+    { label: "Heading 5", value: 5, fontSize: "16px" },
+    { label: "Heading 6", value: 6, fontSize: "14px" },
+  ];
+
+  const getCurrentHeading = () => {
+    for (let level = 1; level <= 5; level++) {
+      if (editor?.isActive("heading", { level })) {
+        return `Heading ${level}`;
+      }
+    }
+
+    return "Normal Text";
+  };
+
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <button className="h-7 min-w-7 shrink-0 flex items-center justify-center rounded-sm hover:bg-neutral-200/80 px-1.5 overflow-hidden text-sm">
+          <span className="truncate">{getCurrentHeading()}</span>
+        </button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent className="p-1 flex flex-col gap-y-1">
+        {headings.map(({ label, value, fontSize }) => (
+          <button
+            onClick={() => {
+              if (value === 0) {
+                editor?.chain().focus().setParagraph().run();
+              } else {
+                editor?.chain().focus().toggleHeading({ level: value as Level }).run();
+              }
+            }}
+            key={label}
+            style={{ fontSize }}
+            className={cn(
+              "flex items-center gap-x-2 px-2 py-1 rounded-sm hover:bg-nuetral-200/80",
+              (value === 0 && !editor?.isActive("heading")) ||
+                (editor?.isActive("heading", { level: value }) &&
+                  "bg-neutral-200/80")
+            )}
+          >
+            {label}
+          </button>
+        ))}
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+};
 
 const FontFamilyButton = () => {
   const { editor } = useEditorStore();
@@ -200,6 +257,7 @@ const Toolbar = () => {
       <FontFamilyButton />
       <Separator orientation="vertical" className="h-6 bg-neutral-400" />
       {/* TODO: Heading */}
+      <HeadlingLevelButton />
       <Separator orientation="vertical" className="h-6 bg-neutral-400" />
       {/* TODO: Font Size */}
       <Separator orientation="vertical" className="h-6 bg-neutral-400" />
